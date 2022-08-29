c2cd8a21190c12b78d5a88d60b37ececedb81444
Daniel <dpenalba@codicesoftware.com>
2022-08-29 16:29:02 +0200

    first commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Avalonia.Controls;
+
+namespace AvaloniaBlurryBorders
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
